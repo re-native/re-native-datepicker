7fb8939723a28787b28b2964d2c495f354f26c26
Fabian Aussems <fabianaussems@gmail.com>
2017-03-17 18:26:10 +0100

    move re-native to re_native

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject re-native/datepicker "1.4.3")
+(defproject re-native/datepicker "1.4.3"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
+                 [re-frame "0.9.1"]]
+  :plugins [[lein-cljsbuild "1.1.4"]])
